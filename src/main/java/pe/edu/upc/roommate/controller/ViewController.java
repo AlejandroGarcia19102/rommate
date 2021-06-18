95c41aa426f8748388f5f03bde952f51c847b730
Brian Obregon <obrian.stark@gmail.com>
2021-06-18 15:04:31 -0500

    Added Layout and Some Views

--- src/main/java/pe/edu/upc/roommate/controller/ViewController.java
+++ src/main/java/pe/edu/upc/roommate/controller/ViewController.java
@@ -1,16 +1,19 @@
 package pe.edu.upc.roommate.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "Hello World");
+@RequestMapping("/")
+public class ViewController {
+    @GetMapping()
+    public String index() {
         return "index";
+    }
+    @GetMapping("about")
+    public String about() {
+        return "about";
     }
 }
 
